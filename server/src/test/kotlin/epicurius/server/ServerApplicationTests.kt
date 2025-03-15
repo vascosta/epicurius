06089c3cc5f1293a81c10b38e5637c0c74bb17aa
Vasco Costa <vascosta15@gmail.com>
2025-03-15 11:37:40 +0000

    *ADDED* Initial Spring MVC project
    Co-Authored-By: Carolina Tavares <113713172+carolinatvs@users.noreply.github.com>

---
+++ server/src/test/kotlin/epicurius/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package epicurius.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
