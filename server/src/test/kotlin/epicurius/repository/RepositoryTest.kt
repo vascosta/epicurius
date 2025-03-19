c555fc6a2eff8a195e41e55144202f99e60a1b40
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-03-19 22:35:54 +0000

    Update user tests

--- server/src/test/kotlin/epicurius/repository/RepositoryTest.kt
+++ server/src/test/kotlin/epicurius/repository/RepositoryTest.kt
@@ -1,22 +1,8 @@
 package epicurius.repository
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.firestore.Firestore
-import com.google.cloud.firestore.FirestoreOptions
-import epicurius.Environment
-import epicurius.domain.UserDomain
-import epicurius.domain.token.Sha256TokenEncoder
-import epicurius.repository.firestore.FirestoreUserRepository
-import epicurius.repository.jdbi.JdbiUserRepository
-import epicurius.repository.jdbi.utils.configure
-import epicurius.repository.transaction.TransactionManager
 import epicurius.repository.transaction.firestore.FirestoreManager
 import epicurius.repository.transaction.jdbi.JdbiTransactionManager
-import org.jdbi.v3.core.Jdbi
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.postgresql.ds.PGSimpleDataSource
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import java.util.UUID
 
 open class RepositoryTest: EpicuriusTest() {
@@ -31,12 +17,11 @@ open class RepositoryTest: EpicuriusTest() {
         @JvmStatic
         @BeforeAll
         fun setupDB() {
-            testUser = createUser(false)
-            testUser2 = createUser(true)
+            testUser = createTestUser(false)
+            testUser2 = createTestUser(true)
         }
 
-
-        private fun createUser(privacy: Boolean): Pair<String, String> {
+        private fun createTestUser(privacy: Boolean): Pair<String, String> {
             val username = "test${Math.random()}"
             val email = "$username@email.com"
             val country = "PT"
@@ -47,6 +32,9 @@ open class RepositoryTest: EpicuriusTest() {
 
             return Pair(username, email)
         }
+
+        fun createUser(username: String, email: String, country: String, passwordHash: String) =
+            tm.run { it.userRepository.createUser(username, email, country, passwordHash) }
 
         fun getUserByName(username: String) = tm.run { it.userRepository.getUser(username, null) }
         fun getUserByEmail(email: String) = tm.run { it.userRepository.getUser(null, email) }
--- server/src/test/kotlin/epicurius/repository/UserRepositoryTest.kt
+++ server/src/test/kotlin/epicurius/repository/UserRepositoryTest.kt
@@ -1,28 +1,32 @@
 package epicurius.repository
 
+import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class UserRepositoryTest: RepositoryTest() {
 
-@Test
-    fun `Getting user by name or email successfully`() {
-        // given a user
-        val user = testUser
+    @Test
+    fun `Create new user and retrieve it successfully`() {
+        // given user required information
+        val username = "test${Math.random()}"
+        val email = "$username@email.com"
+        val country = "PT"
+        val passwordHash = usersDomain.encodePassword(UUID.randomUUID().toString())
+
+        // when creating a user
+        createUser(username, email, country, passwordHash)
 
         // when getting the user by name
-        val userByName = getUserByName(user.first)
+        val userByName = getUserByName(username)
 
         // when getting the user by email
-        val userByEmail = getUserByEmail(user.second)
+        val userByEmail = getUserByEmail(email)
 
         // then the user is retrieved successfully
-        assertEquals(userByName.username, user.first)
-        assertEquals(userByEmail.username, user.first)
-        assertEquals(userByName.email, user.second)
-        assertEquals(userByEmail.email, user.second)
+        assertEquals(userByName.username, username)
+        assertEquals(userByEmail.username, username)
+        assertEquals(userByName.email, email)
+        assertEquals(userByEmail.email, email)
     }
-
-
-
 }
