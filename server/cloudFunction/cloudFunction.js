fdb2091b52decce383d950c14651fb6fd6cde8da
Vasco Costa <vascosta15@gmail.com>
2025-04-21 18:26:27 +0100

    *REFACTORED* local cloud function

--- server/cloudFunction/cloudFunction.js
+++ server/cloudFunction/cloudFunction.js
@@ -5,39 +5,39 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const bucketName = "epicurius-test-bucket";
+const bucketName = "epicurius-bucket";
 const cloudStorageFolder = "ingredients"
 const gcsUri = `gs://${bucketName}/${cloudStorageFolder}/`
 
 const visionClient = new vision.ImageAnnotatorClient();
 
 app.post("/", async (req, res) => {
-    const imageName = req.body.imageName;
 
-    if (!imageName) {
+    const pictureName = req.body.pictureName;
+
+    if (!pictureName) {
         return res.status(400).json({ error: "missing image name" });
     }
 
     try {
-        const [result] = await visionClient.objectLocalization(gcsUri + imageName);
+        const [result] = await visionClient.labelDetection(gcsUri + pictureName);
 
         const ingredients  = [
             ... new Set(
-                result.localizedObjectAnnotations
-                    .map(ingredient => ingredient.name.toLowerCase())
-                    .filter(ingredient => !ingredient.includes(" "))
+                result.labelAnnotations
+                    .map(ingredient => ingredient.description.toLowerCase())
             )
         ];
 
         console.log("Detected Ingredients", ingredients );
 
-        res.status(200).json({ "ingredients": ingredients });
+        res.status(200).json(ingredients);
     } catch (error) {
         res.status(500).json({ "error": error.message });
     }
 });
 
-const PORT = 8080;
+const PORT = 1904;
 app.listen(PORT, () => {
     console.log(`Cloud Function running at http://localhost:${PORT}`);
 });
