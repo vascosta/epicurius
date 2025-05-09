1f79aa49760bd889d6eff3b7f2f23b7194518973
Vasco Costa <vascosta15@gmail.com>
2025-05-09 01:12:29 +0100

    *UPDATED* delete collection

--- server/src/main/kotlin/epicurius/repository/jdbi/collection/JdbiCollectionRepository.kt
+++ server/src/main/kotlin/epicurius/repository/jdbi/collection/JdbiCollectionRepository.kt
@@ -101,8 +101,11 @@ class JdbiCollectionRepository(private val handle: Handle) : CollectionRepositor
     override fun deleteCollection(collectionId: Int) {
         handle.createUpdate(
             """
+                DELETE FROM dbo.collection_recipe
+                WHERE collection_id = :collectionId;
+                
                 DELETE FROM dbo.collection
-                WHERE id = :collectionId
+                WHERE id = :collectionId;
             """
         )
             .bind("collectionId", collectionId)
