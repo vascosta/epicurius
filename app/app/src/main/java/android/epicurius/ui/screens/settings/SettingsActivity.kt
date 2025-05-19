d56ce0c23c8005af71fd48e41525af21dc361899
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-05-19 11:00:04 +0100

    Settings screen draft

---
+++ app/app/src/main/java/android/epicurius/ui/screens/settings/SettingsActivity.kt
@@ -0,0 +1,10 @@
+package android.epicurius.ui.screens.settings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SettingsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
