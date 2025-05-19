84b3798908cae3778cd027eb682801714985200a
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-05-19 11:00:45 +0100

    Added utils

---
+++ app/app/src/main/java/android/epicurius/ui/utils/TextFieldUtils.kt
@@ -0,0 +1,23 @@
+package android.epicurius.ui.utils
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TextField(
+    value: String,
+    label: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(label) },
+        modifier = modifier
+    )
+}
