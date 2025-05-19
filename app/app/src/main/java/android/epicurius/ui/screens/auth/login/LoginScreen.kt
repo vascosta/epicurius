2448429aa3ce4f69d7548ab21b966286c9be7a93
Vasco Costa <vascosta15@gmail.com>
2025-05-19 18:36:49 +0100

    *UPDATE* Login Screen

--- app/app/src/main/java/android/epicurius/ui/screens/auth/login/LoginScreen.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/auth/login/LoginScreen.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +26,10 @@ import androidx.compose.ui.unit.dp
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun LoginScreen() {
+    var username by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
     Scaffold(
         topBar = { TopBar("LogIn") }
     ) {
@@ -33,9 +41,9 @@ fun LoginScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TextField(value = "", onValueChange = {}, label = "Username")
-            TextField(value = "", onValueChange = {}, label = "Email")
-            TextField(value = "", onValueChange = {}, label = "Password")
+            TextField(value = username, onValueChange = { username = it }, label = "Username")
+            TextField(value = email, onValueChange = {email = it}, label = "Email")
+            TextField(value = password, onValueChange = {password = it}, label = "Password")
 
             Row {
                 Button(
