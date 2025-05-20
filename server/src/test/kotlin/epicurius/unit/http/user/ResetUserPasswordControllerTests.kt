4b1e58468893f96542fea3f1be17eba1c10ebe28
Vasco Costa <vascosta15@gmail.com>
2025-05-20 22:51:54 +0100

    *UPDATE* Reset user password controller tests

--- server/src/test/kotlin/epicurius/unit/http/user/ResetUserPasswordControllerTests.kt
+++ server/src/test/kotlin/epicurius/unit/http/user/ResetUserPasswordControllerTests.kt
@@ -1,7 +1,10 @@
 package epicurius.unit.http.user
 
 import epicurius.domain.exceptions.PasswordsDoNotMatch
+import epicurius.domain.exceptions.UserNotFound
 import epicurius.http.controllers.user.models.input.ResetPasswordInputModel
+import epicurius.unit.services.ServiceTest.Companion.resetPassword
+import epicurius.utils.generateEmail
 import epicurius.utils.generateSecurePassword
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -26,6 +29,21 @@ class ResetUserPasswordControllerTests : UserControllerTest() {
         verify(userServiceMock)
             .resetPassword(resetPasswordInputInfo.email, resetPasswordInputInfo.newPassword, resetPasswordInputInfo.confirmPassword)
         assertEquals(HttpStatus.NO_CONTENT, response.statusCode)
+    }
+
+    @Test
+    fun `Should throw UserNotFound when resetting a user's password for a non-existing user`() {
+        // given a non-existing user email
+        val nonExistingUserEmail = generateEmail("nonexistinguser")
+
+        // mock
+        whenever(userServiceMock.resetPassword(nonExistingUserEmail, password, password)).thenThrow(UserNotFound(nonExistingUserEmail))
+
+        // when resetting the password
+        // then the password cannot be reset and throws UserNotFound exception
+        assertFailsWith<UserNotFound> {
+            resetUserPassword(resetPasswordInputInfo.copy(email = nonExistingUserEmail))
+        }
     }
 
     @Test
