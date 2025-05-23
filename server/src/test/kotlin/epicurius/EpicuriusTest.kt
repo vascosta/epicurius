6bf75f07ac5cfcbf93bb3c109a22346f83a89baa
Vasco Costa <vascosta15@gmail.com>
2025-05-24 00:40:30 +0100

    *UPDATE* epicurius and repository test class

--- server/src/test/kotlin/epicurius/EpicuriusTest.kt
+++ server/src/test/kotlin/epicurius/EpicuriusTest.kt
@@ -1,14 +1,8 @@
 package epicurius
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.firestore.Firestore
-import com.google.cloud.firestore.FirestoreOptions
-import epicurius.domain.fridge.FridgeDomain
 import epicurius.domain.picture.PictureDomain
 import epicurius.domain.token.Sha256TokenEncoder
-import epicurius.domain.user.CountriesDomain
 import epicurius.domain.user.UserDomain
-import epicurius.repository.firestore.manager.FirestoreManager
 import epicurius.repository.jdbi.config.configureWithAppRequirements
 import epicurius.repository.transaction.jdbi.JdbiTransactionManager
 import org.jdbi.v3.core.Jdbi
@@ -21,7 +15,7 @@ open class EpicuriusTest {
 
     companion object {
         private const val POSTGRES_TEST_DATABASE_URL = "jdbc:postgresql://localhost/postgres?user=postgres&password=postgres"
-        private const val FIRESTORE_TEST_DATABASE_ID = "epicurius-test-database"
+
         const val GOOGLE_CLOUD_CREDENTIALS_LOCATION = "src/main/resources/epicurius-credentials.json"
 
         val jdbi = Jdbi.create(
@@ -29,9 +23,7 @@ open class EpicuriusTest {
                 setURL(POSTGRES_TEST_DATABASE_URL)
             }
         ).configureWithAppRequirements()
-        private val firestore = getFirestoreService()
         val tm = JdbiTransactionManager(jdbi)
-        val fs = FirestoreManager(firestore)
 
         init {
             jdbi.useHandle<Exception> {
@@ -47,17 +39,6 @@ open class EpicuriusTest {
                 it.execute("DELETE FROM dbo.token")
                 it.execute("DELETE FROM dbo.user")
             }
-        }
-
-        private fun getFirestoreService(): Firestore {
-            val serviceAccount = FileInputStream(GOOGLE_CLOUD_CREDENTIALS_LOCATION)
-
-            val options = FirestoreOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseId(FIRESTORE_TEST_DATABASE_ID)
-                .build()
-
-            return options.service
         }
 
         val userDomain = UserDomain(BCryptPasswordEncoder(), Sha256TokenEncoder())
--- server/src/test/kotlin/epicurius/unit/repository/RepositoryTest.kt
+++ server/src/test/kotlin/epicurius/unit/repository/RepositoryTest.kt
@@ -1,11 +1,14 @@
 package epicurius.unit.repository
 
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.FirestoreOptions
 import com.google.cloud.storage.StorageOptions
 import epicurius.config.CloudStorage
 import epicurius.config.HttpClientConfigurer
 import epicurius.repository.cloudFunction.manager.CloudFunctionManager
 import epicurius.repository.cloudStorage.manager.CloudStorageManager
+import epicurius.repository.firestore.manager.FirestoreManager
 import epicurius.repository.spoonacular.manager.SpoonacularManager
 import epicurius.unit.EpicuriusUnitTest
 import java.io.FileInputStream
@@ -14,10 +17,13 @@ open class RepositoryTest : EpicuriusUnitTest() {
 
     companion object {
         private const val GOOGLE_CLOUD_STORAGE_TEST_BUCKET = "epicurius-test-bucket"
+        private const val FIRESTORE_TEST_DATABASE_ID = "epicurius-test-database"
 
         private val cloudStorage = getCloudStorageService()
         private val httpClient = HttpClientConfigurer()
+        private val firestore = getFirestoreService()
 
+        val fs = FirestoreManager(firestore)
         val cs = CloudStorageManager(cloudStorage)
         val cf = CloudFunctionManager(httpClient)
         val sm = SpoonacularManager(httpClient)
@@ -30,6 +36,17 @@ open class RepositoryTest : EpicuriusUnitTest() {
                 .build()
 
             return CloudStorage(options.service, GOOGLE_CLOUD_STORAGE_TEST_BUCKET)
+        }
+
+        private fun getFirestoreService(): Firestore {
+            val serviceAccount = FileInputStream(GOOGLE_CLOUD_CREDENTIALS_LOCATION)
+
+            val options = FirestoreOptions.newBuilder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseId(FIRESTORE_TEST_DATABASE_ID)
+                .build()
+
+            return options.service
         }
     }
 }
