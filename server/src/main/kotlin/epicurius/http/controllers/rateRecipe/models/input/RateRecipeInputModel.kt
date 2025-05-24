970be90c05b22ecd0c1cb74ac57db11a421994d3
Vasco Costa <vascosta15@gmail.com>
2025-05-24 19:33:27 +0100

    *UPDATE* rate recipe input model

--- server/src/main/kotlin/epicurius/http/controllers/rateRecipe/models/input/RateRecipeInputModel.kt
+++ server/src/main/kotlin/epicurius/http/controllers/rateRecipe/models/input/RateRecipeInputModel.kt
@@ -1,8 +1,8 @@
 package epicurius.http.controllers.rateRecipe.models.input
 
-import epicurius.domain.recipe.RecipeDomain.Companion.MAX_RATING
-import epicurius.domain.recipe.RecipeDomain.Companion.MIN_RATING
-import epicurius.domain.recipe.RecipeDomain.Companion.RATING_MSG
+import epicurius.domain.recipe.utils.Companion.MAX_RATING
+import epicurius.domain.recipe.utils.Companion.MIN_RATING
+import epicurius.domain.recipe.utils.Companion.RATING_MSG
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 
