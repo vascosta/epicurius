3d2d098e3607b5f0103ff207359ed7b9305e3f09
Vasco Costa <vascosta15@gmail.com>
2025-05-24 19:33:15 +0100

    *UPDATE* collection input models

--- server/src/main/kotlin/epicurius/http/controllers/collection/models/input/UpdateCollectionInputModel.kt
+++ server/src/main/kotlin/epicurius/http/controllers/collection/models/input/UpdateCollectionInputModel.kt
@@ -1,8 +1,8 @@
 package epicurius.http.controllers.collection.models.input
 
-import epicurius.domain.collection.CollectionDomain.Companion.COLLECTION_NAME_LENGTH_MSG
-import epicurius.domain.collection.CollectionDomain.Companion.MAX_COLLECTION_NAME_LENGTH
-import epicurius.domain.collection.CollectionDomain.Companion.MIN_COLLECTION_NAME_LENGTH
+import epicurius.domain.collection.utils.Companion.COLLECTION_NAME_LENGTH_MSG
+import epicurius.domain.collection.utils.Companion.MAX_COLLECTION_NAME_LENGTH
+import epicurius.domain.collection.utils.Companion.MIN_COLLECTION_NAME_LENGTH
 import jakarta.validation.constraints.Size
 
 data class UpdateCollectionInputModel(
