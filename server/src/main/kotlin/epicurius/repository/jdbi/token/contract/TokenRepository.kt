c06022b4e71b913c22a010ec177ce9fa24521456
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-05-28 19:11:40 +0100

    *UPDATE* TokenRepository

--- server/src/main/kotlin/epicurius/repository/jdbi/token/contract/TokenRepository.kt
+++ server/src/main/kotlin/epicurius/repository/jdbi/token/contract/TokenRepository.kt
@@ -3,7 +3,6 @@ package epicurius.repository.jdbi.token.contract
 import java.time.LocalDate
 
 interface TokenRepository {
-
     fun createToken(tokenHash: String, lastUsed: LocalDate, userId: Int)
     fun deleteToken(userId: Int)
 }
