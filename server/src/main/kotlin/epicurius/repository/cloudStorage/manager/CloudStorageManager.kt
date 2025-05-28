4170344531663b4e447c55ce4f00eb869693b71f
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-05-28 12:00:58 +0100

    *UPDATE* repository managers anotations
    Co-Authored-By: Carolina Tavares <113713172+carolinatvs@users.noreply.github.com>

--- server/src/main/kotlin/epicurius/repository/cloudStorage/manager/CloudStorageManager.kt
+++ server/src/main/kotlin/epicurius/repository/cloudStorage/manager/CloudStorageManager.kt
@@ -3,8 +3,9 @@ package epicurius.repository.cloudStorage.manager
 import epicurius.config.CloudStorage
 import epicurius.repository.cloudStorage.picture.PictureRepository
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 
-@Component
+@Repository
 class CloudStorageManager(cloudStorage: CloudStorage) {
     val pictureRepository = PictureRepository(cloudStorage)
 }
