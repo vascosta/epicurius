5ceca1872e899344f7cbce48a86dd503b07b6424
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-05-29 17:45:17 +0100

    Added recipe list screen draft

---
+++ app/app/src/main/java/android/epicurius/ui/screens/favourites/list/FavouritesListActivity.kt
@@ -0,0 +1,10 @@
+package android.epicurius.ui.screens.favourites.list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class FavouritesListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
