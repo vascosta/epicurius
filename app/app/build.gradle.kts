f5e49dcc605ec77045c967848793d6b8d4795782
Vasco Costa <vascosta15@gmail.com>
2025-05-29 18:45:32 +0100

    *UPDATE* build gradlew for storage usage

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
 
+    implementation(libs.androidx.datastore.preferences)
+
     implementation(platform(libs.okhttp.bom))
     implementation(libs.okhttp)
     implementation(libs.gson)
