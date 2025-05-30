f36739ce592df8d5825c8618f8d7f1275735828e
Vasco Costa <vascosta15@gmail.com>
2025-05-30 16:44:31 +0100

    *UPDATE* menu controller

--- server/src/main/kotlin/epicurius/http/controllers/menu/MenuController.kt
+++ server/src/main/kotlin/epicurius/http/controllers/menu/MenuController.kt
@@ -2,7 +2,8 @@ package epicurius.http.controllers.menu
 
 import epicurius.domain.user.AuthenticatedUser
 import epicurius.http.controllers.menu.models.out.GetDailyMenuOutputModel
-import epicurius.http.utils.Uris
+import epicurius.http.media.Uris
+import epicurius.http.media.okHttpResponse
 import epicurius.services.menu.MenuService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,8 +19,6 @@ class MenuController(private val menuService: MenuService) {
         authenticatedUser: AuthenticatedUser,
     ): ResponseEntity<*> {
         val dailyMenu = menuService.getDailyMenu(authenticatedUser.user.intolerances, authenticatedUser.user.diets)
-        return ResponseEntity
-            .ok()
-            .body(GetDailyMenuOutputModel(dailyMenu))
+        return okHttpResponse(GetDailyMenuOutputModel(dailyMenu))
     }
 }
