15018feb124c0806dcd03db813d6edf133177916
Vasco Costa <vascosta15@gmail.com>
2025-05-30 14:50:20 +0100

    *UPDATE* create tables

--- server/src/sql/createTables.sql
+++ server/src/sql/createTables.sql
@@ -1,6 +1,6 @@
-create schema dbo
+create schema dbo;
 
-create table dbo.user(
+create table if not exists dbo.user(
     id serial primary key,
     name varchar(25) unique not null,
     email varchar(40) unique not null,
@@ -12,7 +12,7 @@ create table dbo.user(
     profile_picture_name varchar(80)
 );
 
-create table dbo.token(
+create table if not exists dbo.token(
     hash varchar(80) unique not null,
     last_used date,
     user_id int not null,
@@ -20,7 +20,7 @@ create table dbo.token(
     foreign key (user_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.followers(
+create table if not exists dbo.followers(
     user_id int not null,
     follower_id int not null check ( user_id <> follower_id ),
     status int not null,
@@ -28,7 +28,7 @@ create table dbo.followers(
     foreign key (follower_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.fridge(
+create table if not exists dbo.fridge(
     owner_id int not null,
     entry_number serial not null,
     product_name varchar(50) not null,
@@ -39,15 +39,15 @@ create table dbo.fridge(
     foreign key (owner_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.recipe(
+create table if not exists dbo.recipe(
     id serial primary key,
     name varchar(50) not null,
     author_id int not null,
     date date not null,
     servings int not null check ( servings > 0),
     preparation_time int not null check ( preparation_time > 0 ), -- in minutes, e.g. if value is 30, it means 30 minutes
-    meal_type int not null check ( meal_type between 0 and 12), -- breakfast, lunch, dinner, snack
-    cuisine int not null check ( cuisine between 0 and 25), -- italian, mexican, chinese, etc.
+    meal_type int not null check ( meal_type between 0 and 12), -- breakfast, lunch, dinner, snack, etc
+    cuisine int not null check ( cuisine between 0 and 25), -- italian, mexican, chinese, etc
     intolerances int[] not null,
     diets int[] not null,
     calories int check ( calories >= 0),
@@ -58,7 +58,7 @@ create table dbo.recipe(
     foreign key (author_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.recipe_rating(
+create table if not exists dbo.recipe_rating(
     recipe_id int not null,
     user_id int not null, -- cannot be the same as the author
     rating int not null check (rating between 1 and 5),
@@ -68,7 +68,7 @@ create table dbo.recipe_rating(
     foreign key (user_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.ingredient(
+create table if not exists dbo.ingredient(
     recipe_id int not null,
     name varchar(20) not null,
     quantity double precision not null,
@@ -77,7 +77,7 @@ create table dbo.ingredient(
     foreign key (recipe_id) references dbo.recipe(id) on delete cascade
 );
 
-create table dbo.collection(
+create table if not exists dbo.collection(
     id serial primary key,
     owner_id int not null,
     name varchar(30) not null,
@@ -85,7 +85,7 @@ create table dbo.collection(
     foreign key (owner_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.collection_recipe(
+create table if not exists dbo.collection_recipe(
     collection_id int not null,
     recipe_id int not null,
     primary key (collection_id, recipe_id),
@@ -93,7 +93,7 @@ create table dbo.collection_recipe(
     foreign key (recipe_id) references dbo.recipe(id) on delete cascade
 );
 
-create table dbo.meal_planner(
+create table if not exists dbo.meal_planner(
     user_id int not null,
     date date not null,
     max_calories int check ( max_calories >= 0 ),
@@ -101,7 +101,7 @@ create table dbo.meal_planner(
     foreign key (user_id) references dbo.user(id) on delete cascade
 );
 
-create table dbo.meal_planner_recipe(
+create table if not exists dbo.meal_planner_recipe(
     user_id int not null,
     date date not null,
     recipe_id int not null,
@@ -123,7 +123,7 @@ return null;
 end;
 $$ language plpgsql;
 
-create trigger trg_delete_product_with_no_quantity
+create or replace trigger trg_delete_product_with_no_quantity
     after insert or update
     on dbo.fridge
     for each row
