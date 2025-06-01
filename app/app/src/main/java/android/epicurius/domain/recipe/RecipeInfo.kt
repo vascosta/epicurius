62f6754cda2b3f8963ad1185f04f2fa51382a9e0
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-06-01 17:55:22 +0100

    *UPDATE* app recipe info

--- app/app/src/main/java/android/epicurius/domain/recipe/RecipeInfo.kt
+++ app/app/src/main/java/android/epicurius/domain/recipe/RecipeInfo.kt
@@ -3,6 +3,7 @@ package android.epicurius.domain.recipe
 data class RecipeInfo(
     val id: Int,
     val name: String,
+    val authorUsername: String,
     val cuisine: Cuisine,
     val mealType: MealType,
     val preparationTime: Int,
