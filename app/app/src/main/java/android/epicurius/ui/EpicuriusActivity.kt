dbe7d20d174e59a3fb4dbfe232caccf8e98bc8fe
Vasco Costa <vascosta15@gmail.com>
2025-06-02 22:18:05 +0100

    *UPDATE* epicurius activity

--- app/app/src/main/java/android/epicurius/ui/EpicuriusActivity.kt
+++ app/app/src/main/java/android/epicurius/ui/EpicuriusActivity.kt
@@ -1,9 +1,19 @@
 package android.epicurius.ui
 
+import android.content.Context
+import android.epicurius.Dependencies
+import android.epicurius.services.EpicuriusService
+import android.epicurius.storage.Session
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-abstract class EpicuriusActivity : ComponentActivity() {
+open class EpicuriusActivity : ComponentActivity() {
+
+    val dependencies by lazy { application as Dependencies }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,4 +25,26 @@ abstract class EpicuriusActivity : ComponentActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+
+    inline fun <reified T : ViewModel> getViewModel() =
+        createViewModel {
+            T::class.java
+                .getConstructor(
+                    EpicuriusService::class.java,
+                    Session::class.java,
+                    Context::class.java
+                )
+                .newInstance(
+                    dependencies.service,
+                    dependencies.session,
+                    this
+                )
+        }
+
+    inline fun <reified T : ViewModel> ComponentActivity.createViewModel(crossinline block: () -> T) =
+        viewModels<T> {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T = block() as T
+            }
+        }
 }
