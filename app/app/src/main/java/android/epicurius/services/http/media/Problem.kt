2583aa4328f040e7af471d2d35f6753c8ffc4808
Vasco Costa <vascosta15@gmail.com>
2025-06-02 22:18:21 +0100

    *UPDATE* strings xml file

--- app/app/src/main/java/android/epicurius/services/http/media/Problem.kt
+++ app/app/src/main/java/android/epicurius/services/http/media/Problem.kt
@@ -5,6 +5,6 @@ import java.net.URI
 data class Problem(
     val type: URI? = null,
     val title: String? = null,
-    val detail: String? = null,
+    val detail: String,
     val instance: URI? = null
 )
