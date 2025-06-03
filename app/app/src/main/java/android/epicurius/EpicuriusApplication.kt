699d4d6d702860a4eef8231f6b9e3d592afb1ccc
Vasco Costa <vascosta15@gmail.com>
2025-06-03 19:53:52 +0100

    *UPDATE* epicurius application for better context usage

--- app/app/src/main/java/android/epicurius/EpicuriusApplication.kt
+++ app/app/src/main/java/android/epicurius/EpicuriusApplication.kt
@@ -1,6 +1,7 @@
 package android.epicurius
 
 import android.app.Application
+import android.content.Context
 import android.epicurius.services.EpicuriusService
 import android.epicurius.services.http.HttpService
 import android.epicurius.storage.SessionDataStore
@@ -10,15 +11,20 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 
-class EpicuriusApplication: Application(), Dependencies {
-    private val sessionDataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
+
+class EpicuriusApplication : Application(), Dependencies {
     private val gson = Gson()
     private val httpService = HttpService(BASE_URL, OkHttpClient(), gson)
 
     override val service = EpicuriusService(httpService)
-    override val session = SessionDataStore(sessionDataStore, gson)
+    override val session by lazy {
+        SessionDataStore(applicationContext.sessionDataStore, gson)
+    }
 
     companion object {
-        private const val BASE_URL = "localhost:8080/api"
+        private const val BASE_URL = "http://10.0.2.2:8080/api"
     }
 }
+
+val Context.sessionDataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
+
