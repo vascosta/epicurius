762b29562983945cd6ea3998e61d1cf3225b6799
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-06-04 23:03:15 +0100

    *UPDATE* auth viewModel to user viewModel

--- app/app/src/main/java/android/epicurius/ui/screens/auth/login/LoginScreen.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/auth/login/LoginScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.math.log
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
--- app/app/src/main/java/android/epicurius/ui/screens/user/UserViewModel.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/user/UserViewModel.kt
@@ -1,11 +1,26 @@
-package android.epicurius.ui.screens.auth
+package android.epicurius.ui.screens.user
 
 import android.content.Context
+import android.epicurius.domain.user.EMAIL_LENGTH_MSG
+import android.epicurius.domain.user.MAX_EMAIL_LENGTH
+import android.epicurius.domain.user.MAX_PASSWORD_LENGTH
+import android.epicurius.domain.user.MAX_USERNAME_LENGTH
+import android.epicurius.domain.user.MIN_EMAIL_LENGTH
+import android.epicurius.domain.user.MIN_PASSWORD_LENGTH
+import android.epicurius.domain.user.MIN_USERNAME_LENGTH
+import android.epicurius.domain.user.PASSWORD_LENGTH_MSG
+import android.epicurius.domain.user.USERNAME_LENGTH_MSG
+import android.epicurius.domain.user.VALID_EMAIL_MSG
+import android.epicurius.domain.user.VALID_PASSWORD_MSG
+import android.epicurius.domain.user.VALID_USERNAME_MSG
+import android.epicurius.domain.user.emailRegex
+import android.epicurius.domain.user.passwordRegex
+import android.epicurius.domain.user.usernameRegex
 import android.epicurius.services.EpicuriusService
 import android.epicurius.storage.Session
 import android.epicurius.ui.EpicuriusViewModel
 
-open class AuthViewModel(
+open class UserViewModel(
     service: EpicuriusService,
     session: Session,
     context: Context
@@ -58,28 +73,5 @@ open class AuthViewModel(
             return false
         }
         return true
-    }
-
-    companion object {
-        const val MIN_USERNAME_LENGTH = 3
-        const val MAX_USERNAME_LENGTH = 25
-
-        const val MIN_EMAIL_LENGTH = 3
-        const val MAX_EMAIL_LENGTH = MAX_USERNAME_LENGTH + 30
-
-        const val MIN_PASSWORD_LENGTH = 8
-        const val MAX_PASSWORD_LENGTH = 30
-
-        val usernameRegex = Regex("(?=.*[a-zA-Z])[a-zA-Z0-9]*$")
-        const val VALID_USERNAME_MSG = "username must have at least one letter and one number"
-        const val USERNAME_LENGTH_MSG = "username must be between $MIN_USERNAME_LENGTH and $MAX_USERNAME_LENGTH characters"
-
-        val passwordRegex = Regex("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()\\-__+.]).*$")
-        const val VALID_PASSWORD_MSG = "password must have uppercase and lowercase letters, at least a number and a special character"
-        const val PASSWORD_LENGTH_MSG = "password must be between $MIN_PASSWORD_LENGTH and $MAX_PASSWORD_LENGTH characters"
-
-        val emailRegex = Regex("^[A-Za-z0-9+_.-]+@(.+)\$")
-        const val VALID_EMAIL_MSG = "email must be a valid email address"
-        const val EMAIL_LENGTH_MSG = "email must be between $MIN_EMAIL_LENGTH and $MAX_EMAIL_LENGTH characters"
     }
 }
