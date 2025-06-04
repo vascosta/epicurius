ff9035e77a61a29aff119058ff570908783c6e1f
Vasco Costa <vascosta15@gmail.com>
2025-06-05 00:01:51 +0100

    *UPDATE* favourites list activity for viewModel

--- app/app/src/main/java/android/epicurius/ui/screens/favourites/list/FavouritesListActivity.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/favourites/list/FavouritesListActivity.kt
@@ -1,33 +1,47 @@
 package android.epicurius.ui.screens.favourites.list
 
 import android.epicurius.MainActivity
-import android.epicurius.domain.recipe.Cuisine
-import android.epicurius.domain.recipe.MealType
-import android.epicurius.domain.recipe.RecipeInfo
+import android.epicurius.ui.EpicuriusActivity
+import android.epicurius.ui.Intents
+import android.epicurius.ui.screens.favourites.folder.FavouritesActivity
+import android.epicurius.ui.screens.utils.Idle
+import android.epicurius.ui.screens.utils.idle
 import android.epicurius.ui.screens.utils.navigateTo
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 
-class FavouritesListActivity : ComponentActivity() {
+class FavouritesListActivity : EpicuriusActivity() {
+    val viewModel: FavouritesListViewModel by getViewModel<FavouritesListViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            combine(
+                viewModel.recipes,
+                viewModel.favouritesListName
+            ) { recipesState, favouritesNameState -> recipesState to favouritesNameState }
+                .collectLatest { (recipesState, favouritesNameState) ->
+                    val recipeId = intent.getIntExtra(Intents.RECIPE_ID, -1)
+                    if (recipesState is Idle || favouritesNameState is Idle) {
+                        viewModel.getCollection(recipeId) { navigateTo<FavouritesActivity>() }
+                    }
+                }
+        }
         setContent {
+            val recipes = viewModel.recipes.collectAsState(idle())
+            val favouritesListName = viewModel.favouritesListName.collectAsState(idle())
             FavouritesListScreen(
                 onBackButton = { navigateTo<MainActivity>() },
-                folderName = "My Favourite Recipes",
-                recipeList = listOf(
-                    RecipeInfo(
-                        id = 1,
-                        name = "Spaghetti Carbonara",
-                        authorUsername = "ChefBear",
-                        cuisine = Cuisine.ITALIAN,
-                        mealType = MealType.MAIN_COURSE,
-                        preparationTime = 35,
-                        servings = 4,
-                        picture = "".toByteArray()
-                    )
-                )
+                onFavouritesRefresh = {
+                    val recipeId = intent.getIntExtra(Intents.RECIPE_ID, -1)
+                    viewModel.getCollection(recipeId) { navigateTo<FavouritesActivity>() }
+                },
+                favouritesListNameState = favouritesListName.value,
+                recipesState = recipes.value
             )
         }
     }
