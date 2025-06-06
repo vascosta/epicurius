d2d732efeb7ac7caea9234db9cd719643894435d
Vasco Costa <vascosta15@gmail.com>
2025-06-06 17:36:25 +0100

    *ADD* username intent

--- app/app/src/main/java/android/epicurius/ui/navigation/Intents.kt
+++ app/app/src/main/java/android/epicurius/ui/navigation/Intents.kt
@@ -3,4 +3,5 @@ package android.epicurius.ui.navigation
 object Intents {
     const val COLLECTION_ID = "CollectionIdIntent"
     const val RECIPE_ID = "RecipeIdIntent"
+    const val USERNAME = "UsernameIntent"
 }
