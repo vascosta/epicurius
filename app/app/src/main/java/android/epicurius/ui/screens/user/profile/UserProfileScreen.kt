760a50c3d34351fb3f02ea89435fbb4d3ba81d68
Vasco Costa <vascosta15@gmail.com>
2025-06-06 19:43:59 +0100

    *UPDATE* user profile screen for viewModel

--- app/app/src/main/java/android/epicurius/ui/screens/user/profile/UserProfileScreen.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/user/profile/UserProfileScreen.kt
@@ -6,6 +6,9 @@ import android.epicurius.ui.navigation.TopBar
 import android.epicurius.ui.screens.user.components.FollowBox
 import android.epicurius.ui.screens.user.components.ProfileTabBar
 import android.epicurius.ui.screens.user.components.UserProfilePicture
+import android.epicurius.ui.screens.utils.LoadState
+import android.epicurius.ui.screens.utils.LoadStateRenderer
+import android.epicurius.ui.screens.utils.apiSuccess
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -28,43 +31,73 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun UserProfileScreen(
-    onBackButton: () -> Unit = {},
-    userProfile: UserProfile
+    isAnotherUserProfile: Boolean,
+    userProfileVisibility: Boolean,
+    //followEnable: Boolean,
+    onBackButton: () -> Unit,
+    onSettingsButton: () -> Unit,
+    //onFollow: (String) -> Unit,
+    //onUnfollow: (String) -> Unit,
+    onCollectionRequest: (Int) -> Unit,
+    //onRecipeRequest: (Int) -> Unit,
+    //onAddRecipeToCollectionRequest: (Int, Int) -> Unit,
+    onUserProfileRefresh: () -> Unit,
+    onUserRecipesLoadMore: () -> Unit,
+    onUserKitchenBookLoadMore: () -> Unit,
+    userProfileState: LoadState<UserProfile>,
+    //userRecipesState: LoadState<List<RecipeInfo>>,
+    //userKitchenBookState: LoadState<List<CollectionProfile>>
+    //kitchenBookCollectionRecipesState: LoadState<List<RecipeInfo>>
 ) {
     Scaffold(
         topBar = {
-            TopBar(text = "Profile", backButton = true, onBackButton = onBackButton, icon = Icons.Filled.Settings)
+            TopBar(
+                text = "Profile",
+                backButton = true,
+                onBackButton = onBackButton,
+                icon = if (!isAnotherUserProfile) Icons.Filled.Settings else null,
+                onIconClick = onSettingsButton
+            )
         },
         bottomBar = { BottomBar() },
         content = { paddingValues ->
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .background(Color.White),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+            LoadStateRenderer(
+                loadState = userProfileState,
+                swipeToRefresh = onUserProfileRefresh,
+                content = { userProfile ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .background(Color.White),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(modifier = Modifier.fillMaxHeight(0.02f))
 
-                UserProfilePicture(userProfile.profilePicture, 120)
+                        UserProfilePicture(userProfile.profilePicture, 120)
 
-                Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+                        Spacer(modifier = Modifier.fillMaxHeight(0.02f))
 
-                Text(text = userProfile.name, fontWeight = FontWeight.Bold)
+                        Text(text = userProfile.name, fontWeight = FontWeight.Bold)
 
-                Spacer(modifier = Modifier.fillMaxHeight(0.05f))
+                        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
 
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    FollowBox("Followers", userProfile.followersCount)
-                    FollowBox("Following", userProfile.followingCount)
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                        ) {
+                            FollowBox("Followers", userProfile.followersCount)
+                            FollowBox("Following", userProfile.followingCount)
+                        }
+
+                        Spacer(modifier = Modifier.fillMaxHeight(0.03f))
+
+                        if (userProfileVisibility) {
+                            ProfileTabBar()
+                        }
+
+                    }
                 }
-
-                Spacer(modifier = Modifier.fillMaxHeight(0.03f))
-
-                ProfileTabBar()
-            }
+            )
         }
     )
 }
@@ -81,5 +114,5 @@ fun UserProfilePreview() {
         followingCount = 50
     )
 
-    UserProfileScreen(userProfile = userProfile)
+    UserProfileScreen(false, true, {}, {}, {}, {}, {}, {}, apiSuccess(userProfile))
 }
