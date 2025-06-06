b1cdd3eb86f718941be9cf298f14f39d8119ab7a
Vasco Costa <vascosta15@gmail.com>
2025-06-06 17:14:29 +0100

    *UPDATE* collection service and controller tests

--- server/src/test/kotlin/epicurius/unit/http/collection/GetCollectionsControllerTests.kt
+++ server/src/test/kotlin/epicurius/unit/http/collection/GetCollectionsControllerTests.kt
@@ -1,6 +1,5 @@
 package epicurius.unit.http.collection
 
-import epicurius.domain.PagingParams
 import epicurius.domain.collection.CollectionProfile
 import epicurius.domain.collection.CollectionType
 import epicurius.http.controllers.collection.models.output.GetCollectionsOutputModel
@@ -14,18 +13,18 @@ class GetCollectionsControllerTests : CollectionControllerTest() {
 
     @Test
     fun `Should retrieve multiple collections from a user successfully`() {
-        // given a collection type and paging parameters
+        // given a collection type
         val collectionType = CollectionType.KITCHEN_BOOK
-        val pagingParams = PagingParams(0, 10)
+        val limit = 1
 
         // mock
         val mockCollectionProfile = CollectionProfile(1, "Test Collection")
         whenever(
-            collectionServiceMock.getCollections(testPublicAuthenticatedUser.user.id, collectionType, pagingParams)
+            collectionServiceMock.getCollections(testPublicAuthenticatedUser.user.id, collectionType, null, limit)
         ).thenReturn(listOf(mockCollectionProfile))
 
         // when retrieving the collection
-        val response = getCollections(testPublicAuthenticatedUser, collectionType, pagingParams.skip, pagingParams.limit)
+        val response = getCollections(testPublicAuthenticatedUser, collectionType, null, limit)
         val body = response.body as GetCollectionsOutputModel
 
         // then the collection is retrieved successfully
--- server/src/test/kotlin/epicurius/unit/services/collection/GetCollectionsServiceTests.kt
+++ server/src/test/kotlin/epicurius/unit/services/collection/GetCollectionsServiceTests.kt
@@ -1,6 +1,5 @@
 package epicurius.unit.services.collection
 
-import epicurius.domain.PagingParams
 import epicurius.domain.collection.CollectionType
 import epicurius.repository.jdbi.collection.models.JdbiCollectionProfileModel
 import org.mockito.kotlin.whenever
@@ -11,17 +10,17 @@ class GetCollectionsServiceTests : CollectionServiceTest() {
 
     @Test
     fun `Should retrieve multiple collections from a user successfully`() {
-        // given a collection type and paging parameters
+        // given a collection type
         val collectionType = CollectionType.FAVOURITE
-        val pagingParams = PagingParams(0, 10)
+        val limit = 10
 
         // mock
         val mockJdbiCollectionProfileModel = JdbiCollectionProfileModel(1, "Test Collection")
-        whenever(jdbiCollectionRepositoryMock.getCollections(testPublicUser.id, collectionType, pagingParams))
+        whenever(jdbiCollectionRepositoryMock.getCollections(testPublicUser.id, collectionType, null, limit))
             .thenReturn(listOf(mockJdbiCollectionProfileModel))
 
         // when retrieving the collections
-        val collections = getCollections(testPublicUser.id, collectionType, pagingParams)
+        val collections = getCollections(testPublicUser.id, collectionType, null, limit)
 
         // then the collection is retrieved successfully
         assertNotNull(collections.find { it.id == mockJdbiCollectionProfileModel.id })
