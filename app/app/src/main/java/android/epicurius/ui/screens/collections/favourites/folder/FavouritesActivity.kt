5aec1a365bf6858a7cd83c48a36451a9daf26dc8
Vasco Costa <vascosta15@gmail.com>
2025-06-07 20:57:28 +0100

    *UPDATE* favourites activity

--- app/app/src/main/java/android/epicurius/ui/screens/collections/favourites/folder/FavouritesActivity.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/collections/favourites/folder/FavouritesActivity.kt
@@ -4,6 +4,7 @@ import android.epicurius.ui.EpicuriusActivity
 import android.epicurius.ui.navigation.Intents
 import android.epicurius.ui.screens.collections.favourites.list.FavouritesListActivity
 import android.epicurius.ui.screens.user.profile.UserProfileActivity
+import android.epicurius.ui.screens.user.settings.SettingsActivity
 import android.epicurius.ui.screens.utils.Idle
 import android.epicurius.ui.screens.utils.idle
 import android.epicurius.ui.screens.utils.navigateTo
@@ -21,7 +22,7 @@ class FavouritesActivity : EpicuriusActivity() {
         super.onCreate(savedInstanceState)
         lifecycleScope.launch {
             viewModel.favourites.collectLatest { state ->
-                if (state is Idle) viewModel.getFavourites()
+                if (state is Idle) viewModel.getFavourites { navigateTo<SettingsActivity>() }
             }
         }
         setContent {
@@ -34,7 +35,7 @@ class FavouritesActivity : EpicuriusActivity() {
                 },
                 onCollectionRequest = ::navigateToFavouritesListActivity,
                 onCollectionDelete = { id -> viewModel.deleteFavouriteCollection(id) },
-                onFavouritesRefresh = { viewModel.getFavourites() },
+                onFavouritesRefresh = { viewModel.getFavourites { navigateTo<SettingsActivity>() } },
                 enableButtons = viewModel.enableButtons,
             )
         }
