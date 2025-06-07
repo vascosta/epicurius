3132b07e7e3632e758ba8895c5cae4dd0c13a270
Vasco Costa <vascosta15@gmail.com>
2025-06-07 09:56:36 +0100

    *UPDATE* signUp activity
    Add on back button leave app

--- app/app/src/main/java/android/epicurius/ui/screens/auth/signup/SignUpActivity.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/auth/signup/SignUpActivity.kt
@@ -1,19 +1,26 @@
 package android.epicurius.ui.screens.auth.signup
 
-import android.epicurius.MainActivity
 import android.epicurius.ui.EpicuriusActivity
 import android.epicurius.ui.screens.auth.login.LoginActivity
 import android.epicurius.ui.screens.feed.FeedActivity
 import android.epicurius.ui.screens.utils.navigateTo
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 
 class SignUpActivity : EpicuriusActivity() {
     val viewmodel: SignUpViewModel by getViewModel<SignUpViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finishAffinity()
+            }
+        })
+
         setContent {
             MaterialTheme {
                 SignUpScreen(
