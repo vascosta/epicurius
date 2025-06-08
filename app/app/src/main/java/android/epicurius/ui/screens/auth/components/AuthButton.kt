a1cf68448b6d568338dd6ae0f6cf9074692aa4ac
Vasco Costa <vascosta15@gmail.com>
2025-06-08 13:46:15 +0100

    *UPDATE* auth button

--- app/app/src/main/java/android/epicurius/ui/screens/auth/components/AuthButton.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/auth/components/AuthButton.kt
@@ -1,6 +1,8 @@
 package android.epicurius.ui.screens.auth.components
 
+import android.epicurius.ui.screens.utils.LoadingSpinner
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,10 +10,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun AuthButton(label: String, onClick: () -> Unit, enabled: Boolean) {
+fun AuthButton(
+    onClick: () -> Unit,
+    enabled: Boolean,
+    label: String,
+) {
     Button(
         onClick = { onClick() },
         modifier = Modifier.padding(10.dp),
         enabled = enabled
-    ) { Text(label) }
+    ) {
+        if (enabled) {
+            Text(label)
+        }
+        else {
+            LoadingSpinner(Modifier.size(30.dp))
+        }
+    }
 }
