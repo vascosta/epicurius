d0b1f10d21639872361320bcd137ac7ab1a2ea10
Vasco Costa <vascosta15@gmail.com>
2025-06-09 00:06:19 +0100

    *UPDATE* navigateTo function
    Add clear flags for onBack navigation

--- app/app/src/main/java/android/epicurius/ui/navigation/ActivityNavigation.kt
+++ app/app/src/main/java/android/epicurius/ui/navigation/ActivityNavigation.kt
@@ -4,8 +4,15 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.ComponentActivity
 
-inline fun <reified T : ComponentActivity> Context.navigateTo(configIntent: (Intent) -> Unit = {}) {
+inline fun <reified T : ComponentActivity> Context.navigateTo(
+    useFlags: Boolean = false,
+    configIntent: (Intent) -> Unit = {}
+) {
     val intent = Intent(this, T::class.java)
+    if (useFlags) {
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+    }
     configIntent(intent)
     startActivity(intent)
 }
+
