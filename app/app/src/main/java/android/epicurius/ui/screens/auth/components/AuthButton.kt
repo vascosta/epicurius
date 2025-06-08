013092073b107c396598d7d4015c67a75a39dfbd
Vasco Costa <vascosta15@gmail.com>
2025-06-08 16:18:02 +0100

    *UPDATE* auth button
    Update loading spinner to show only when a button is clicked

--- app/app/src/main/java/android/epicurius/ui/screens/auth/components/AuthButton.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/auth/components/AuthButton.kt
@@ -6,6 +6,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,16 +20,21 @@ fun AuthButton(
     enabled: Boolean,
     label: String,
 ) {
+    var showLoadingSpinner by remember { mutableStateOf(!enabled) }
+
     Button(
-        onClick = { onClick() },
+        onClick = {
+            onClick()
+            showLoadingSpinner = true
+        },
         modifier = Modifier.padding(10.dp),
         enabled = enabled
     ) {
-        if (enabled) {
+        if (!showLoadingSpinner || enabled) {
             Text(label)
         }
         else {
-            LoadingSpinner(Modifier.size(30.dp))
+            LoadingSpinner(Modifier.size(25.dp))
         }
     }
 }
