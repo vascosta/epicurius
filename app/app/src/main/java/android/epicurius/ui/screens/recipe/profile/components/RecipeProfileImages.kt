fc882babf7035750fcdbe0ddceeb91cd463e9dc0
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-06-10 00:54:17 +0100

    Added recipe image update

--- app/app/src/main/java/android/epicurius/ui/screens/recipe/profile/components/RecipeProfileImages.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/recipe/profile/components/RecipeProfileImages.kt
@@ -1,6 +1,8 @@
 package android.epicurius.ui.screens.recipe.profile.components
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,16 +11,19 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeProfileImages(
-    images: List<Int>,
-    pagerState: PagerState
+    images: List<ByteArray>,
+    pagerState: PagerState,
+    onImageClick: () -> Unit,
+    enabled: Boolean
 ) {
     HorizontalPager(
         state = pagerState,
@@ -27,13 +32,21 @@ fun RecipeProfileImages(
             .padding(5.dp)
             .height(250.dp)
     ) { page ->
+        val bitmap = remember(images[page]) {
+            BitmapFactory.decodeByteArray(images[page], 0, images[page].size)
+        }
         Image(
-            painter = painterResource(id = images[page]),
+            bitmap = bitmap.asImageBitmap(),
             contentDescription = "Recipe Image",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxSize()
                 .clip(RoundedCornerShape(12.dp))
+                .clickable(
+                    enabled = enabled,
+                    onClick = { onImageClick() }
+                )
         )
     }
 }
+
