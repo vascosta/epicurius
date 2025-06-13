7fd36248bc8f8b953c0a0a768ef20f59ff78913e
Vasco Costa <vascosta15@gmail.com>
2025-06-13 18:31:17 +0100

    *REMOVE* firestore usage

--- server/src/main/kotlin/epicurius/repository/firestore/recipe/models/FirestoreRecipeModel.kt
+++
@@ -1,5 +0,0 @@
-package epicurius.repository.firestore.recipe.models
-
-import epicurius.domain.recipe.Instructions
-
-data class FirestoreRecipeModel(val id: Int, val description: String, val instructions: Instructions)
--- server/src/main/kotlin/epicurius/repository/firestore/recipe/models/FirestoreUpdateRecipeModel.kt
+++
@@ -1,5 +0,0 @@
-package epicurius.repository.firestore.recipe.models
-
-import epicurius.domain.recipe.Instructions
-
-data class FirestoreUpdateRecipeModel(val id: Int, val description: String?, val instructions: Instructions?)
