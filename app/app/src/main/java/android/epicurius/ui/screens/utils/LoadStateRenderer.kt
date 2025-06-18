2779b4c76b410f98b8a8bee5887c33f5a27ad502
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-06-18 22:56:15 +0100

    *UPDATE* load state renderer

--- app/app/src/main/java/android/epicurius/ui/screens/utils/LoadStateRenderer.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/utils/LoadStateRenderer.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun <T> LoadStateRenderer(
     loadState: LoadState<T>,
-    swipeToRefresh: (() -> Unit)? = null,
     content: @Composable (T) -> Unit = {},
 ) {
     when (loadState) {
@@ -18,13 +17,11 @@ fun <T> LoadStateRenderer(
                 when {
                     loadState.cachedValue.isSuccess -> RenderSuccess(
                         loadState.getOrThrow(),
-                        swipeToRefresh,
                         { value ->
                             content(value)
                             LoadingSpinner(Modifier.size(30.dp))
                         }
                     )
-                    loadState.cachedValue.isFailure -> RenderFailure(swipeToRefresh)
                 }
             }
             else {
@@ -34,8 +31,7 @@ fun <T> LoadStateRenderer(
         }
         is Loaded -> {
             when {
-                loadState.value.isSuccess -> RenderSuccess(loadState.getOrThrow(), swipeToRefresh, content)
-                loadState.value.isFailure -> RenderFailure(swipeToRefresh)
+                loadState.value.isSuccess -> RenderSuccess(loadState.getOrThrow(), content)
             }
         }
     }
@@ -44,44 +40,7 @@ fun <T> LoadStateRenderer(
 @Composable
 fun <T> RenderSuccess(
     value: T,
-    swipeToRefresh: (() -> Unit)?,
     content: @Composable (T) -> Unit = {}
 ) {
-    if (swipeToRefresh != null) {
-        content(value)
-/*        PullToRefresh(onRefresh = pullToRefresh) {
-            content(value)
-        }*/
-    } else {
-        content(value)
-    }
-}
-
-@Composable
-fun RenderFailure(
-    swipeToRefresh: (() -> Unit)?
-) {
-    /*if (pullToRefresh != null) {
-        PullToRefresh(onRefresh = pullToRefresh) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Refresh,
-                    contentDescription = stringResource(R.string.pull_to_refresh_icon),
-                    modifier = Modifier.size(35.dp),
-                    tint = MaterialTheme.colorScheme.secondary
-                )
-                Spacer(10.dp)
-                Text(
-                    text = stringResource(R.string.pull_to_refresh),
-                    color = MaterialTheme.colorScheme.secondary
-                )
-            }
-        }
-    }*/
+    content(value)
 }
