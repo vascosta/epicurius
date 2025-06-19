be5e82efcbabb44d44c0bca5a3c79def464354ef
Vasco Costa <101342648+vascosta@users.noreply.github.com>
2025-06-19 16:09:25 +0100

    *UPDATE* create recipe viewModel

--- app/app/src/main/java/android/epicurius/domain/recipe/utils.kt
+++ app/app/src/main/java/android/epicurius/domain/recipe/utils.kt
@@ -1,5 +1,7 @@
 package android.epicurius.domain.recipe
 
+import android.epicurius.domain.Picture
+
 const val MIN_RECIPE_NAME_LENGTH = 3
 const val MAX_RECIPE_NAME_LENGTH = 50
 const val RECIPE_NAME_LENGTH_MSG = "recipe name must be between $MIN_RECIPE_NAME_LENGTH and $MAX_RECIPE_NAME_LENGTH characters"
@@ -40,3 +42,124 @@ fun getIngredientQuantityMessage(name: Double) =
 
 fun getInstructionStepMessage(stepIdentifier: String) =
     "the length of the $stepIdentifier step must be between $MIN_INSTRUCTIONS_STEP_LENGTH and $MAX_INSTRUCTIONS_STEP_LENGTH characters"
+
+fun validateName(
+    name: String,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (name.length !in MIN_RECIPE_NAME_LENGTH..MAX_RECIPE_NAME_LENGTH) {
+        showErrorMessage(RECIPE_NAME_LENGTH_MSG)
+        return false
+    }
+    return true
+}
+
+fun validateDescription(
+    description: String,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (description.length > MAX_RECIPE_DESCRIPTION_LENGTH) {
+        showErrorMessage(RECIPE_DESCRIPTION_LENGTH_MSG)
+        return false
+    }
+    return true
+}
+
+fun validateServings(
+    servings: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(servings, "servings", showErrorMessage)
+
+fun validatePreparationTime(
+    preparationTime: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(preparationTime, "preparation time", showErrorMessage)
+
+fun validateIngredients(
+    ingredients: List<Ingredient>,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (ingredients.size > MAX_NUMBER_OF_INGREDIENTS) {
+        showErrorMessage(INGREDIENTS_SIZE_MSG)
+        return false
+    }
+
+    ingredients.forEach { ingredient ->
+        if (ingredient.name.length !in MIN_INGREDIENT_NAME_LENGTH..MAX_INGREDIENT_NAME_LENGTH) {
+            showErrorMessage(getIngredientNameMessage(ingredient.name))
+            return false
+        }
+
+        if (ingredient.quantity < MIN_INGREDIENT_QUANTITY) {
+            showErrorMessage(getIngredientQuantityMessage(ingredient.quantity))
+            return false
+        }
+    }
+    return true
+}
+
+fun validateCalories(
+    calories: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(calories, "calories", showErrorMessage)
+
+fun validateProtein(
+    protein: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(protein, "protein", showErrorMessage)
+
+fun validateFat(
+    fat: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(fat, "fat", showErrorMessage)
+
+fun validateCarbs(
+    carbs: Int,
+    showErrorMessage: (message: String) -> Unit
+): Boolean = validateNumber(carbs, "carbs", showErrorMessage)
+
+fun validateInstructions(
+    instructions: Instructions,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (instructions.steps.size > MAX_NUMBER_OF_INSTRUCTIONS_STEPS) {
+        showErrorMessage(INSTRUCTIONS_STEPS_SIZE_MSG)
+        return false
+    }
+
+    instructions.steps.forEach { (stepNumber, stepDescription) ->
+        if (stepNumber.toIntOrNull() == null) {
+            showErrorMessage(INSTRUCTIONS_STEP_NUMBER_MSG)
+            return false
+        }
+
+        if (stepDescription.length !in MIN_INSTRUCTIONS_STEP_LENGTH..MAX_INSTRUCTIONS_STEP_LENGTH) {
+            showErrorMessage(getInstructionStepMessage(stepNumber))
+            return false
+        }
+    }
+    return true
+}
+
+fun validatePictures(
+    pictures: List<Picture>,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (pictures.size !in MIN_NUMBER_OF_RECIPE_PICTURES..MAX_NUMBER_OF_RECIPE_PICTURES) {
+        showErrorMessage(RECIPE_PICTURES_MSG)
+        return false
+    }
+    return true
+}
+
+private fun validateNumber(
+    number: Int,
+    recipeParamName: String,
+    showErrorMessage: (message: String) -> Unit
+): Boolean {
+    if (number < 0) {
+        showErrorMessage(getPositiveNumberMessage(recipeParamName))
+        return false
+    }
+    return true
+}
--- app/app/src/main/java/android/epicurius/ui/screens/recipe/createRecipe/CreateRecipeViewModel.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/recipe/createRecipe/CreateRecipeViewModel.kt
@@ -8,10 +8,21 @@ import android.epicurius.domain.recipe.Cuisine
 import android.epicurius.domain.recipe.Ingredient
 import android.epicurius.domain.recipe.Instructions
 import android.epicurius.domain.recipe.MealType
+import android.epicurius.domain.recipe.validateCalories
+import android.epicurius.domain.recipe.validateCarbs
+import android.epicurius.domain.recipe.validateDescription
+import android.epicurius.domain.recipe.validateFat
+import android.epicurius.domain.recipe.validateIngredients
+import android.epicurius.domain.recipe.validateInstructions
+import android.epicurius.domain.recipe.validateName
+import android.epicurius.domain.recipe.validatePictures
+import android.epicurius.domain.recipe.validatePreparationTime
+import android.epicurius.domain.recipe.validateProtein
+import android.epicurius.domain.recipe.validateServings
 import android.epicurius.services.EpicuriusService
 import android.epicurius.services.api.recipe.models.input.CreateRecipeInputModel
 import android.epicurius.storage.Session
-import android.epicurius.ui.screens.recipe.RecipeViewModel
+import android.epicurius.ui.EpicuriusViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
@@ -19,7 +30,7 @@ class CreateRecipeViewModel(
     service: EpicuriusService,
     session: Session,
     context: Context
-): RecipeViewModel(service, session, context) {
+): EpicuriusViewModel(service, session, context) {
 
     fun createRecipe(
         name: String,
@@ -53,7 +64,7 @@ class CreateRecipeViewModel(
                 carbs,
                 instructions
             ) ||
-            !validatePictures(pictures)
+            !validatePictures(pictures, ::showToast)
         ) {
             enableButtons()
             return
@@ -110,14 +121,14 @@ class CreateRecipeViewModel(
         instructions: Instructions,
     ): Boolean =
         when {
-            !validateName(name) || !validateDescription(description) ||
-                 !validateServings(servings) || !validatePreparationTime(preparationTime) ||
-                         !validateIngredients(ingredients) || !validateInstructions(instructions)
+            !validateName(name, ::showToast) || !validateDescription(description, ::showToast) ||
+                    !validateServings(servings, ::showToast) || !validatePreparationTime(preparationTime, ::showToast) ||
+                         !validateIngredients(ingredients, ::showToast) || !validateInstructions(instructions, ::showToast)
                              -> false
-            calories != null && !validateCalories(calories) -> false
-            protein != null && !validateProtein(protein) -> false
-            fat != null && !validateFat(fat) -> false
-            carbs != null && !validateCarbs(carbs) -> false
+            calories != null && !validateCalories(calories, ::showToast) -> false
+            protein != null && !validateProtein(protein, ::showToast) -> false
+            fat != null && !validateFat(fat, ::showToast) -> false
+            carbs != null && !validateCarbs(carbs, ::showToast) -> false
             else -> true
         }
 
