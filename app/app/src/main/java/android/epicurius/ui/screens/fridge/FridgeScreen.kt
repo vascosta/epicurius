053d19d1a18f7e29764aaee619ccba62720b107b
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-06-28 18:42:31 +0100

    Updated fridge screen

--- app/app/src/main/java/android/epicurius/ui/screens/fridge/FridgeScreen.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/fridge/FridgeScreen.kt
@@ -1,5 +1,6 @@
 package android.epicurius.ui.screens.fridge
 
+import android.annotation.SuppressLint
 import android.epicurius.domain.fridge.Product
 import android.epicurius.ui.navigation.BottomBar
 import android.epicurius.ui.navigation.TopBar
@@ -11,9 +12,16 @@ import android.epicurius.ui.screens.utils.Loaded
 import android.epicurius.ui.screens.utils.apiSuccess
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -22,17 +30,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import java.time.LocalDate
 import java.time.Period
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun FridgeScreen(
     userFridgeState: LoadState<List<Product>>,
@@ -55,70 +68,92 @@ fun FridgeScreen(
 ) {
     var showAddProductDialog by remember { mutableStateOf(false) }
 
-    Scaffold(
-        topBar = {
-            TopBar(
-                titleText = "Fridge",
-                backButton = true,
-                onBackButton = onBackButton,
-                enableButtons = enableButtons
-            )
-        },
-        bottomBar = { BottomBar(buttonsEnable = enableButtons) },
-        floatingActionButton = {
-            FloatingActionButton(
-                modifier = Modifier.clickable(enabled = enableButtons) { showAddProductDialog = true },
-                onClick = {},
-            ) {
-                Icon(Icons.Default.Add, contentDescription = "Add Product")
-            }
-        },
-        containerColor = Color.White
-    ) { paddingValues ->
-        LoadStateRenderer(
-            loadState = userFridgeState,
-            content = { products ->
-                if (products.isNotEmpty()) {
-                    LazyColumn(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .background(Color.White),
-                        contentPadding = paddingValues
-                    ) {
-                        items(products) { product ->
-                            ProductItemCard(
-                                product = product,
-                                onUpdateProduct = onUpdateProduct,
-                                onDeleteProduct = onDeleteProduct,
-                                enableButtons = enableButtons
+    BoxWithConstraints {
+        val maxHeight = constraints.maxHeight.toFloat()
+
+        var offsetX by remember { mutableFloatStateOf(0f) }
+        var offsetY by remember { mutableFloatStateOf(0f) }
+
+        Scaffold(
+            topBar = {
+                TopBar(
+                    titleText = "Fridge",
+                    backButton = true,
+                    onBackButton = onBackButton,
+                    enableButtons = enableButtons
+                )
+            },
+            bottomBar = { BottomBar(buttonsEnable = enableButtons) },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { showAddProductDialog = true },
+                    modifier = Modifier
+                        .offset { IntOffset(offsetX.toInt(), offsetY.toInt()) }
+                        .pointerInput(Unit) {
+                            detectDragGestures { change, dragAmount ->
+                                change.consume()
+                                offsetY = (offsetY + dragAmount.y)
+                                    .coerceIn( - (maxHeight - 232.dp.toPx()), 0f)
+                            }
+                        }
+                ) {
+                    Icon(Icons.Default.Add, contentDescription = "Add Product")
+                }
+            },
+            containerColor = Color.White
+        ) { paddingValues ->
+            LoadStateRenderer(
+                loadState = userFridgeState,
+                content = { products ->
+                    if (products.isNotEmpty()) {
+                        LazyColumn(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .background(Color.White),
+                            contentPadding = paddingValues
+                        ) {
+                            items(products) { product ->
+                                ProductItemCard(
+                                    product = product,
+                                    onUpdateProduct = onUpdateProduct,
+                                    onDeleteProduct = onDeleteProduct,
+                                    enableButtons = enableButtons
+                                )
+                            }
+                        }
+                    } else if (userFridgeState is Loaded) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues)
+                                .padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Your fridge is empty!",
+                                modifier = Modifier.padding(10.dp),
+                                textAlign = TextAlign.Center,
+                                color = Color.Gray
                             )
                         }
                     }
-                } else if (userFridgeState is Loaded) {
-                    Text(
-                        text = "Your fridge is empty!",
-                        modifier = Modifier.padding(10.dp),
-                        textAlign = TextAlign.Center,
-                        color = Color.Gray
-                    )
+                    if (showAddProductDialog) {
+                        AddProductDialog(
+                            onAddProduct = onAddProduct,
+                            onDismiss = { showAddProductDialog = false },
+                            enableButtons = enableButtons
+                        )
+                    }
                 }
-                if (showAddProductDialog) {
-                    AddProductDialog(
-                        onAddProduct = onAddProduct,
-                        onDismiss = { showAddProductDialog = false },
-                        enableButtons = enableButtons
-                    )
-                }
-            }
-        )
-
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewFridgeScreen() {
-    val sampleProducts = listOf(
+    val sampleProducts = listOf<Product>(
         Product("Milk", 1, 2, LocalDate.now().minusDays(1), LocalDate.now().plusDays(5)),
         Product("Eggs", 2, 12, null, LocalDate.now().plusDays(10)),
         Product("Meat", 3, 1, LocalDate.now().minusDays(2), LocalDate.now().minusDays(1)),
