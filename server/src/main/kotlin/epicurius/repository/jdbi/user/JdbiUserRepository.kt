48c575574f545a449fe97ad39d8d995a25e24f7e
Vasco Costa <vascosta15@gmail.com>
2025-07-09 23:26:47 +0100

    fix: user search in followers and following

--- server/src/main/kotlin/epicurius/repository/jdbi/user/JdbiUserRepository.kt
+++ server/src/main/kotlin/epicurius/repository/jdbi/user/JdbiUserRepository.kt
@@ -104,7 +104,7 @@ class JdbiUserRepository(private val handle: Handle) : UserRepository {
                 FROM dbo.user u
                 JOIN dbo.followers f ON u.id = f.follower_id
                 WHERE f.user_id = :user_id 
-                    AND (LOWER(u.name) LIKE LOWER(:partialFollowerName) OR :partialFollowerName IS NULL)
+                    AND LOWER(u.name) LIKE LOWER(:partialFollowerName)
                     AND f.status = :status
                     AND (:lastFollowerId IS NULL OR f.follower_id < :lastFollowerId)
                 ORDER BY u.id DESC
@@ -112,7 +112,7 @@ class JdbiUserRepository(private val handle: Handle) : UserRepository {
             """
         )
             .bind("user_id", userId)
-            .bind("partialFollowerName", "%$partialFollowerName%")
+            .bind("partialFollowerName", "%${partialFollowerName ?: ""}%")
             .bind("status", FollowingStatus.ACCEPTED.ordinal)
             .bind("lastFollowerId", lastFollowerId)
             .bind("limit", limit)
@@ -145,7 +145,7 @@ class JdbiUserRepository(private val handle: Handle) : UserRepository {
                 FROM dbo.user u
                 JOIN dbo.followers f ON u.id = f.user_id
                 WHERE f.follower_id = :user_id 
-                    AND (LOWER(u.name) LIKE LOWER(:partialFollowingName) OR :partialFollowingName IS NULL)
+                    AND LOWER(u.name) LIKE LOWER(:partialFollowingName)
                     AND f.status = :status
                     AND (:lastFollowingId IS NULL OR f.user_id < :lastFollowingId)
                 ORDER BY u.id DESC
@@ -153,7 +153,7 @@ class JdbiUserRepository(private val handle: Handle) : UserRepository {
             """
         )
             .bind("user_id", userId)
-            .bind("partialFollowingName", "%$partialFollowingName%")
+            .bind("partialFollowingName", "%${partialFollowingName ?: ""}%")
             .bind("status", FollowingStatus.ACCEPTED.ordinal)
             .bind("lastFollowingId", lastFollowingId)
             .bind("limit", limit)
