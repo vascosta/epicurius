5c336a17f9c54a07cafef11eb2c9d8e206083a4a
49465 - Carolina Tavares <a49465@alunos.isel.pt>
2025-07-17 12:19:28 +0100

    Updated server offline ui

--- app/app/src/main/java/android/epicurius/ui/screens/serverOffline/ServerOfflineScreen.kt
+++ app/app/src/main/java/android/epicurius/ui/screens/serverOffline/ServerOfflineScreen.kt
@@ -1,11 +1,16 @@
 package android.epicurius.ui.screens.serverOffline
 
+import android.epicurius.R
 import android.epicurius.ui.navigation.BottomBar
 import android.epicurius.ui.navigation.TopBar
+import android.epicurius.ui.screens.theme.Lilac
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -15,6 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,32 +39,38 @@ fun ServerOfflineScreen() {
         },
         bottomBar = { BottomBar(buttonsEnable = false) },
         content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-                    .padding(16.dp)
-                    .background(Color.White)
-                    .verticalScroll(rememberScrollState()),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "Server is Offline",
-                    modifier = Modifier.padding(bottom = 16.dp),
-                    color = Color.Black,
-                    fontSize = 24.sp,
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = R.drawable.background_server_offline),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
                 )
-                Text(
-                    text = "In the meantime try to think about new recipes to share!",
-                    modifier = Modifier.padding(bottom = 8.dp),
-                    color = Color.Gray,
-                    fontSize = 16.sp,
-                    textAlign = TextAlign.Center
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Server is Offline",
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        color = Lilac,
+                        fontSize = 24.sp,
+                    )
+                    Text(
+                        text = "In the meantime try to think about new recipes to share!",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
-        },
-        containerColor = Color.White
+        }
     )
 }
 
